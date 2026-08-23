7e7d86348d89f08002b2a6494d6da20863812a17
ram16 <thomass.montaine@gmail.com>
2026-08-23 15:30:36 +0200

    yes

--- Editor/Shitcord.cs
+++ Editor/Shitcord.cs
@@ -1,222 +1,189 @@
-﻿//#if UNITY_EDITOR
-//using System;
-//using System.IO.Pipes;
-//using _ARK_;
-//using Discord.Sdk;
-//using UnityEditor;
-//using UnityEngine;
+﻿using System;
+using _ARK_;
+using Discord.Sdk;
+using UnityEditor;
+using UnityEngine;
 
-//namespace _DISCORD_
-//{
-//    [InitializeOnLoad]
-//    static class ShitDiscord
-//    {
-//        const ulong application_id = 1029411129065222204;
-//        static Client client;
-//        static bool is_initialized;
-//        static PlayModeStateChange playModeState;
+namespace _DISCORD_
+{
+    [InitializeOnLoad]
+    static partial class Shitcord
+    {
+        static Client client;
+        static bool is_initialized;
+        static PlayModeStateChange playModeState;
 
-//        //----------------------------------------------------------------------------------------------------------
+        //----------------------------------------------------------------------------------------------------------
 
-//        static ShitDiscord()
-//        {
-//            Debug.Log($"{typeof(ShitDiscord)}.CONSTRUCTOR");
+        static Shitcord()
+        {
+            Debug.Log($"{typeof(Shitcord)}.CONSTRUCTOR");
 
-//            AssemblyReloadEvents.beforeAssemblyReload += StopPresence;
-//            AssemblyReloadEvents.afterAssemblyReload += StartPresence;
-//            EditorApplication.quitting += StopPresence;
-//            EditorApplication.update += RunCallbacks;
-//            EditorApplication.delayCall += StartPresence;
+            AssemblyReloadEvents.beforeAssemblyReload += StopPresence;
+            AssemblyReloadEvents.afterAssemblyReload += StartPresence;
+            EditorApplication.quitting += StopPresence;
+            EditorApplication.update += RunCallbacks;
+            EditorApplication.delayCall += StartPresence;
 
-//            EditorApplication.playModeStateChanged += static value =>
-//            {
-//                playModeState = value;
-//                UpdatePresence();
-//            };
-//        }
+            EditorApplication.playModeStateChanged += static value =>
+            {
+                playModeState = value;
+                UpdatePresence();
+            };
+        }
 
-//        //----------------------------------------------------------------------------------------------------------
+        //----------------------------------------------------------------------------------------------------------
 
-//        [MenuItem("Assets/" + nameof(_DISCORD_) + "/" + nameof(StartPresence))]
-//        [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.AfterSceneLoad)]
-//        static void StartPresence()
-//        {
-//            if (is_initialized)
-//            {
-//                UpdatePresence();
-//                return;
-//            }
+        [MenuItem("Assets/" + nameof(_DISCORD_) + "/" + nameof(StartPresence))]
+        [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.AfterSceneLoad)]
+        static void StartPresence()
+        {
+            LoadText(true);
 
-//            Debug.Log($"{typeof(ShitDiscord)}.START_PRESENCE");
+            if (is_initialized)
+            {
+                UpdatePresence();
+                return;
+            }
 
-//            try
-//            {
-//                client = new Client();
-//                client.SetApplicationId(application_id);
-//                client.SetStatusChangedCallback(OnClientStatusChanged);
-//                client.GetDiscordClientConnectedUser(application_id, OnDiscordClientConnectedUser);
-//                is_initialized = true;
-//            }
-//            catch (Exception exception) when (exception is DllNotFoundException || exception is EntryPointNotFoundException)
-//            {
-//                Debug.LogException(exception);
-//                client?.Dispose();
-//                client = null;
-//                return;
-//            }
+            Debug.Log($"{typeof(Shitcord)}.START_PRESENCE");
 
-//            UpdatePresence();
-//        }
+            try
+            {
+                client = new Client();
+                client.SetApplicationId(application_id);
+                client.SetStatusChangedCallback(OnClientStatusChanged);
+                client.GetDiscordClientConnectedUser(application_id, OnDiscordClientConnectedUser);
+                is_initialized = true;
+            }
+            catch (Exception exception) when (exception is DllNotFoundException || exception is EntryPointNotFoundException)
+            {
+                Debug.LogException(exception);
+                client?.Dispose();
+                client = null;
+                return;
+            }
 
-//        [MenuItem("Assets/" + nameof(_DISCORD_) + "/" + nameof(UpdatePresence))]
-//        static void UpdatePresence()
-//        {
-//            if (!is_initialized || client == null)
-//                return;
+            UpdatePresence();
+        }
 
-//            Debug.Log($"{typeof(ShitDiscord)}.UPDATE_PRESENCE");
+        [MenuItem("Assets/" + nameof(_DISCORD_) + "/" + nameof(UpdatePresence))]
+        static void UpdatePresence()
+        {
+            if (!is_initialized || client == null)
+                return;
 
-//            using Activity activity = new();
-//            using ActivityTimestamps timestamps = new();
+            Debug.Log($"{typeof(Shitcord)}.UPDATE_PRESENCE");
 
-//            timestamps.SetStart((ulong)NUCLEOR.timestamp_editorStart.ToUnixTimeSeconds());
+            using Activity activity = new();
+            using ActivityTimestamps timestamps = new();
 
-//            activity.SetName("𝐒 𝐇 𝐈 𝐓 𝐒 𝐓 𝐎 𝐑 𝐌");
-//            activity.SetType(ActivityTypes.Playing);
-//            activity.SetStatusDisplayType(StatusDisplayTypes.Name);
-//            activity.SetDetails("Unity " + Application.unityVersion);
-//            activity.SetState($"{playModeState}");
-//            activity.SetTimestamps(timestamps);
+            timestamps.SetStart((ulong)NUCLEOR.timestamp_editorStart.ToUnixTimeSeconds());
 
-//            client.UpdateRichPresence(activity, OnRichPresenceUpdated);
-//        }
+            if (string.IsNullOrEmpty(application_name))
+                activity.SetName("𝐒 𝐇 𝐈 𝐓 𝐒 𝐓 𝐎 𝐑 𝐌");
+            else
+                activity.SetName(application_name);
 
-//        [MenuItem("Assets/" + nameof(_DISCORD_) + "/" + nameof(StopPresence))]
-//        static void StopPresence()
-//        {
-//            if (!is_initialized || client == null)
-//                return;
+            activity.SetType(ActivityTypes.Playing);
+            activity.SetStatusDisplayType(StatusDisplayTypes.Name);
 
-//            Debug.Log($"{typeof(ShitDiscord)}.SHUTDOWN");
+            if (show_unityVersion)
+                activity.SetDetails("Unity " + Application.unityVersion);
 
-//            try
-//            {
-//                client.ClearRichPresence();
-//                RunCallbacks();
-//                client.Dispose();
-//            }
-//            finally
-//            {
-//                client = null;
-//                is_initialized = false;
-//            }
-//        }
+            if (show_unityState)
+                activity.SetState($"{playModeState}");
 
-//        [MenuItem("Assets/" + nameof(_DISCORD_) + "/" + nameof(RestartPresence))]
-//        static void RestartPresence()
-//        {
-//            StopPresence();
-//            StartPresence();
-//        }
+            activity.SetTimestamps(timestamps);
 
-//        [MenuItem("Assets/" + nameof(_DISCORD_) + "/" + nameof(ProbeDiscordRpcPipes))]
-//        static void ProbeDiscordRpcPipes()
-//        {
-//#if UNITY_EDITOR_WIN
-//            bool found_pipe = false;
+            client.UpdateRichPresence(activity, OnRichPresenceUpdated);
+        }
 
-//            for (int i = 0; i < 10; i++)
-//            {
-//                string pipe_name = $"discord-ipc-{i}";
+        [MenuItem("Assets/" + nameof(_DISCORD_) + "/" + nameof(StopPresence))]
+        static void StopPresence()
+        {
+            if (!is_initialized || client == null)
+                return;
 
-//                try
-//                {
-//                    using NamedPipeClientStream pipe = new(".", pipe_name, PipeDirection.InOut);
-//                    pipe.Connect(100);
+            Debug.Log($"{typeof(Shitcord)}.SHUTDOWN");
 
-//                    if (pipe.IsConnected)
-//                    {
-//                        found_pipe = true;
-//                        Debug.Log($"{typeof(ShitDiscord)}.PIPE_OK {pipe_name}");
-//                    }
-//                }
-//                catch (TimeoutException)
-//                {
-//                    Debug.Log($"{typeof(ShitDiscord)}.PIPE_MISS {pipe_name}");
-//                }
-//                catch (Exception exception)
-//                {
-//                    Debug.LogWarning($"{typeof(ShitDiscord)}.PIPE_ERROR {pipe_name} {exception.GetType().Name}: {exception.Message}");
-//                }
-//            }
+            try
+            {
+                client.ClearRichPresence();
+                RunCallbacks();
+                client.Dispose();
+            }
+            finally
+            {
+                client = null;
+                is_initialized = false;
+            }
+        }
 
-//            if (!found_pipe)
-//            {
-//                Debug.LogWarning($"{typeof(ShitDiscord)}.NO_DISCORD_RPC_PIPE_FOUND. Discord desktop is likely not exposing RPC to the Unity editor.");
-//            }
-//#else
-//            Debug.LogWarning($"{typeof(ShitDiscord)}.{nameof(ProbeDiscordRpcPipes)} is only implemented for the Windows Unity editor.");
-//#endif
-//        }
+        [MenuItem("Assets/" + nameof(_DISCORD_) + "/" + nameof(RestartPresence))]
+        static void RestartPresence()
+        {
+            StopPresence();
+            StartPresence();
+        }
 
-//        //----------------------------------------------------------------------------------------------------------
+        //----------------------------------------------------------------------------------------------------------
 
-//        static void RunCallbacks()
-//        {
-//            if (!is_initialized || client == null)
-//            {
-//                return;
-//            }
+        static void RunCallbacks()
+        {
+            if (!is_initialized || client == null)
+            {
+                return;
+            }
 
-//            NativeMethods.Discord_RunCallbacks();
-//        }
+            NativeMethods.Discord_RunCallbacks();
+        }
 
-//        static void OnClientStatusChanged(Client.Status status, Client.Error error, int errorDetail)
-//        {
-//            Debug.Log($"{typeof(ShitDiscord)}.STATUS status={status} error={error} detail={errorDetail}");
-//        }
+        static void OnClientStatusChanged(Client.Status status, Client.Error error, int errorDetail)
+        {
+            Debug.Log($"{typeof(Shitcord)}.STATUS status={status} error={error} detail={errorDetail}");
+        }
 
-//        static void OnRichPresenceUpdated(ClientResult result)
-//        {
-//            try
-//            {
-//                if (result.Successful())
-//                {
-//                    Debug.Log($"{typeof(ShitDiscord)}.RICH_PRESENCE_UPDATED {result}");
-//                    return;
-//                }
+        static void OnRichPresenceUpdated(ClientResult result)
+        {
+            try
+            {
+                if (result.Successful())
+                {
+                    Debug.Log($"{typeof(Shitcord)}.RICH_PRESENCE_UPDATED {result}");
+                    return;
+                }
 
-//                if (result == null)
-//                    Debug.LogError($"{typeof(ShitDiscord)}.RICH_PRESENCE_UPDATE_FAILED {nameof(result)}={result}");
-//                else
-//                    Debug.LogError($"{typeof(ShitDiscord)}.RICH_PRESENCE_UPDATE_FAILED type={result.Type()} status={result.Status()} code={result.ErrorCode()} error={result.Error()} details={result}");
-//            }
-//            finally
-//            {
-//                result.Dispose();
-//            }
-//        }
+                if (result == null)
+                    Debug.LogError($"{typeof(Shitcord)}.RICH_PRESENCE_UPDATE_FAILED {nameof(result)}={result}");
+                else
+                    Debug.LogError($"{typeof(Shitcord)}.RICH_PRESENCE_UPDATE_FAILED type={result.Type()} status={result.Status()} code={result.ErrorCode()} error={result.Error()} details={result}");
+            }
+            finally
+            {
+                result.Dispose();
+            }
+        }
 
-//        static void OnDiscordClientConnectedUser(ClientResult result, UserHandle user)
-//        {
-//            try
-//            {
-//                if (!result.Successful())
-//                {
-//                    Debug.LogWarning($"{typeof(ShitDiscord)}.CLIENT_USER_UNAVAILABLE type={result.Type()} status={result.Status()} code={result.ErrorCode()} error={result.Error()} details={result}");
-//                    return;
-//                }
-//                else if (user == null)
-//                    Debug.LogWarning($"{typeof(ShitDiscord)}.CLIENT_USER_NULL");
-//                else
-//                    Debug.Log($"{typeof(ShitDiscord)}.CLIENT_USER display_name={user.DisplayName()} id={user.Id()}");
-//            }
-//            finally
-//            {
-//                user?.Dispose();
-//                result.Dispose();
-//            }
-//        }
-//    }
-//}
-//#endif
+        static void OnDiscordClientConnectedUser(ClientResult result, UserHandle user)
+        {
+            try
+            {
+                if (!result.Successful())
+                {
+                    Debug.LogWarning($"{typeof(Shitcord)}.CLIENT_USER_UNAVAILABLE type={result.Type()} status={result.Status()} code={result.ErrorCode()} error={result.Error()} details={result}");
+                    return;
+                }
+                else if (user == null)
+                    Debug.LogWarning($"{typeof(Shitcord)}.CLIENT_USER_NULL");
+                else
+                    Debug.Log($"{typeof(Shitcord)}.CLIENT_USER display_name={user.DisplayName()} id={user.Id()}");
+            }
+            finally
+            {
+                user?.Dispose();
+                result.Dispose();
+            }
+        }
+    }
+}
---
+++ Editor/_Texts.cs
@@ -0,0 +1,54 @@
+﻿using _ARK_;
+using Newtonsoft.Json.Linq;
+using System.IO;
+using UnityEditor;
+using UnityEngine;
+
+namespace _DISCORD_
+{
+    partial class Shitcord
+    {
+        static ulong application_id;
+        static string application_name;
+        static bool show_unityVersion;
+        static bool show_unityState;
+
+        static string GetTextPath() => Path.Combine(ArkMachine.DFResources.FullName, typeof(Shitcord).GetJSonFileName());
+
+        //----------------------------------------------------------------------------------------------------------
+
+        [MenuItem("Assets/" + nameof(_DISCORD_) + "/" + nameof(OpenText))]
+        static void OpenText() => Application.OpenURL(GetTextPath());
+
+        static void SaveText()
+        {
+            string spath = GetTextPath();
+            JObject jobj = new()
+            {
+                [nameof(application_id)] = application_id,
+                [nameof(application_name)] = application_name,
+                [nameof(show_unityVersion)] = show_unityVersion,
+                [nameof(show_unityState)] = show_unityState,
+            };
+            jobj.NJSave(spath);
+        }
+
+        static void LoadText(in bool log)
+        {
+            string lpath = GetTextPath();
+
+            if (!Util.TryNJRead(lpath, out JObject jobj, force: true, log_success: log))
+            {
+                SaveText();
+                Application.OpenURL(lpath);
+            }
+            else
+            {
+                jobj.TryRead(nameof(application_id), ref application_id);
+                jobj.TryRead(nameof(application_name), ref application_name);
+                jobj.TryRead(nameof(show_unityVersion), ref show_unityVersion);
+                jobj.TryRead(nameof(show_unityState), ref show_unityState);
+            }
+        }
+    }
+}
